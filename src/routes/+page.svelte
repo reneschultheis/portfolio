4fd7abe5f3db0a5d13b1bcc89af94335a232a3e8
René Schultheis <schultheisrene@gmail.com>
2023-09-17 22:29:57 +0200

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,39 @@
+<div
+    class="grid gap-2 auto-rows-fr lg:grid-cols-5 md:grid-cols-5 sm:grid-cols-3 xs:grid-cols-1"
+>
+    <div class="bg-gray-100 p-2 rounded-md">
+        <h1 class="text-lg">Hi!</h1>
+        <p>My name is René.</p>
+    </div>
+    <div class="bg-gray-100 p-2 rounded-md">
+        <p>
+            3 years into development i love creating beautiful, minimalistic
+            stuff
+        </p>
+    </div>
+    <div class="bg-gray-100 p-2 rounded-md">
+        <p>Currently i am working on a new project called</p>
+    </div>
+    <div class="bg-gray-100 p-2 rounded-md row-span-2" />
+    <div class="bg-gray-100 p-2 rounded-md" />
+</div>
+<div
+    class="grid grid-flow-col gap-2 auto-rows-fr lg:grid-cols-5 md:grid-cols-5 sm:grid-cols-3 xs:grid-cols-1"
+>
+    <div class="bg-gray-100 p-2 rounded-md row-span-2">
+        <h1 class="text-lg">The Sign</h1>
+        <p>
+            A simple and easy to use reservation tool. User login and cookie
+            handling of the users data.
+        </p>
+    </div>
+    <div class="bg-gray-100 p-2 rounded-md row-span-1">
+        <p>React - MUI - Supabase</p>
+    </div>
+    <div class="bg-blue-100 p-2 rounded-md flex flex-1 test">
+        <a class="flex flex-1 text-blue-500 text-center items-center" target="_blank" href="https://thesignreservation.vercel.app">thesignreservationtool.com</a>
+    </div>
+    <div class="bg-gray-100 rounded-md row-span-2">
+        <img class="rounded-md" src="../../thesignreservationtool.png" />
+    </div>
+</div>
