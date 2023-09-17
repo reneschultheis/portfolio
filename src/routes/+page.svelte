cd159c2a147304bce5e892e8ed9435ff142993e0
René Schultheis <schultheisrene@gmail.com>
2023-09-17 22:38:35 +0200

    Changed col styling

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -18,20 +18,20 @@
     <div class="bg-gray-100 p-2 rounded-md" />
 </div>
 <div
-    class="grid grid-flow-col gap-2 auto-rows-fr lg:grid-cols-5 md:grid-cols-5 sm:grid-cols-3 xs:grid-cols-1"
+    class="grid mt-2 gap-2  auto-rows-max lg:grid-cols-5 md:grid-cols-5 sm:grid-cols-3 xs:grid-cols-1"
 >
-    <div class="bg-gray-100 p-2 rounded-md row-span-2">
+    <div class="bg-gray-100 p-2 rounded-md row-span-1">
         <h1 class="text-lg">The Sign</h1>
         <p>
             A simple and easy to use reservation tool. User login and cookie
             handling of the users data.
         </p>
     </div>
-    <div class="bg-gray-100 p-2 rounded-md row-span-1">
-        <p>React - MUI - Supabase</p>
+    <div class="bg-gray-100 p-2 rounded-md">
+        <p class="my-6">React - MUI - Supabase</p>
     </div>
     <div class="bg-blue-100 p-2 rounded-md flex flex-1 test">
-        <a class="flex flex-1 text-blue-500 text-center items-center" target="_blank" href="https://thesignreservation.vercel.app">thesignreservationtool.com</a>
+        <a class="flex flex-1 my-6 text-blue-500 text-center items-center" target="_blank" href="https://thesignreservation.vercel.app">thesignreservationtool.com</a>
     </div>
     <div class="bg-gray-100 rounded-md row-span-2">
         <img class="rounded-md" src="../../thesignreservationtool.png" />
